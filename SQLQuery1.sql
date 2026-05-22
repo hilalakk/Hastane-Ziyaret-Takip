ef3d477b28557927eb9b6e589fac593f24e9c888
hilalakk <hilalak015@gmail.com>
2026-05-22 23:09:58 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE Hasta
+(
+    HastaID INT PRIMARY KEY IDENTITY(1,1),
+    Ad NVARCHAR(50),
+    Soyad NVARCHAR(50),
+    TCNo NVARCHAR(11),
+    OdaNo NVARCHAR(10)
+)
+
+CREATE TABLE Ziyaretci
+(
+    ZiyaretciID INT PRIMARY KEY IDENTITY(1,1),
+    Ad NVARCHAR(50),
+    Soyad NVARCHAR(50),
+    TCNo NVARCHAR(11)
+)
+
+CREATE TABLE Ziyaret
+(
+    ZiyaretID INT PRIMARY KEY IDENTITY(1,1),
+    HastaID INT,
+    ZiyaretciID INT,
+    GirisSaati DATETIME,
+    CikisSaati DATETIME NULL,
+
+    FOREIGN KEY (HastaID)
+    REFERENCES Hasta(HastaID),
+
+    FOREIGN KEY (ZiyaretciID)
+    REFERENCES Ziyaretci(ZiyaretciID)
+)
